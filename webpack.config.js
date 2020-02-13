7d4e8d7012cf3dac2127ffa9b0eae181a9fa35e9
KuArthur <48932889+KuArthur@users.noreply.github.com>
2020-02-14 02:30:55 +0300

    made two buttons

--- webpack.config.js
+++ webpack.config.js
@@ -7,7 +7,7 @@ module.exports = {
 
   entry: './src/index.js',
   output: {
-    filename: 'main.js',
+    filename: '[name].js',
     path: path.resolve(__dirname, 'dist'),
     //publicpath: '/dist'
 
