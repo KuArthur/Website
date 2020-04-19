bbe4658db83826be94d4d46589d16af060abf2eb
KuArthur <48932889+KuArthur@users.noreply.github.com>
2020-04-19 23:48:53 +0300

    сделал кривой dropdown

--- src/blocks/counter/counter.js
+++ src/blocks/counter/counter.js
@@ -1,28 +1,27 @@
 import $ from 'jquery';
 import "jquery-ui";
 
-$(document).ready(function(){
-    $('button').click(function(e){
-        var button_classes, value = +$('.counter__input').val();
-        button_classes = $(e.currentTarget).prop('class');        
-        if(button_classes.indexOf('counter__button-plus') !== -1){
-            value = (value) + 1;            
+$(document).ready(function() {
+    $('button').click(function(e) {
+        var button_classes, value = +$('.js-counter__input').val();
+        button_classes = $(e.currentTarget).prop('class');
+        if (button_classes.indexOf('js-counter__button-plus') !== -1) {
+            value = (value) + 1;
         } else {
-            value = (value) - 1;            
+            value = (value) - 1;
         }
         value = value < 0 ? 0 : value;
         $('.counter__input').val(value);
-        if(value !== 0) {
-            $(".counter__button-minus").removeClass("counter__button_status-disabled");
+        if (value !== 0) {
+            $(".js-counter__button-minus").removeClass("counter__button_status-disabled");
         } else {
-            $(".counter__button-minus").addClass("counter__button_status-disabled");
+            $(".js-counter__button-minus").addClass("counter__button_status-disabled");
         }
-        
-    });  
-    $('.counter__input').click(function(){
+
+    });
+    $('.js-counter__input').click(function() {
         $(this).focus().select();
 
     });
-    
 
 });
--- webpack.config.js
+++ webpack.config.js
@@ -6,76 +6,81 @@ const path = require('path')
 
 module.exports = {
 
-  entry: {
-    index: './src/index.js',
-    formElements: './src/pages/form-elements/form-elements.js',
-    colorAndTypes: './src/pages/color-and-types/color-and-types.js'
-  },
-  output: {
-    filename: '[name].js',
-    path: path.resolve(__dirname, 'dist'),
-    //publicpath: '/dist'
+    entry: {
+        index: './src/index.js',
+        formElements: './src/pages/form-elements/form-elements.js',
+        colorAndTypes: './src/pages/color-and-types/color-and-types.js'
+    },
+    output: {
+        filename: '[name].js',
+        path: path.resolve(__dirname, 'dist'),
+        //publicpath: '/dist'
 
-  },
-  module: {
-    rules: [
-      {
-        test: /\.s[ac]ss$/i,
-        use: [
-          // Creates `style` nodes from JS strings
-          'style-loader',
-          // Translates CSS into CommonJS
-          'css-loader',
-          // Compiles Sass to CSS
-          'sass-loader',
+    },
+    module: {
+        rules: [{
+                test: /\.s[ac]ss$/i,
+                use: [
+                    // Creates `style` nodes from JS strings
+                    'style-loader',
+                    // Translates CSS into CommonJS
+                    'css-loader',
+                    // Compiles Sass to CSS
+                    'sass-loader',
+                ],
+            },
+            {
+                test: /\.pug$/,
+                loader: 'pug-loader'
+            },
+            {
+                test: /\.jpe?g$|\.gif$|\.png$|\.svg$|\.woff$|\.ttf$|\.wav$|\.mp3$/,
+                loader: "file-loader"
+            },
+            {
+                // ESLint
+                enforce: 'pre',
+                test: /\.js$/,
+                include: path.resolve(__dirname, 'src'),
+                loader: 'eslint-loader',
+            },
+            {
+                test: /\.m?js$/,
+                exclude: /(node_modules|bower_components)/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: [
+                            '@babel/preset-env'
+                        ]
+                    }
+                }
+            }
         ],
-      },
-      {
-        test: /\.pug$/,
-        loader: 'pug-loader'
-      },
-      {
-        test: /\.jpe?g$|\.gif$|\.png$|\.svg$|\.woff$|\.ttf$|\.wav$|\.mp3$/,
-        loader: "file-loader"
-      },
-      {
-        test: /\.m?js$/,
-        exclude: /(node_modules|bower_components)/,
-        use: {
-          loader: 'babel-loader',
-          options: {
-            presets: [
-                '@babel/preset-env'
-        ]
-          }
-        }
-      }
-    ],
-  },
-  devServer: {
-    overlay: true //вывод ошибок в браузере
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      filename: "index.html",
-      template: './src/index.pug'
-    }),
-    new HtmlWebpackPlugin({
-      filename: "pages/form-elements.html",
-      template:'./src/pages/form-elements/form-elements.pug'
-    }),
-    new HtmlWebpackPlugin({
-      filename: "pages/color-and-types.html",
-      template:'./src/pages/color-and-types/color-and-types.pug'
-    }),
-    
-    new webpack.ProvidePlugin({
-      $: 'jquery',
-      jQuery: 'jquery',
-      'window.jQuery': 'jquery'
+    },
+    devServer: {
+        overlay: true //вывод ошибок в браузере
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            filename: "index.html",
+            template: './src/index.pug'
+        }),
+        new HtmlWebpackPlugin({
+            filename: "pages/form-elements.html",
+            template: './src/pages/form-elements/form-elements.pug'
+        }),
+        new HtmlWebpackPlugin({
+            filename: "pages/color-and-types.html",
+            template: './src/pages/color-and-types/color-and-types.pug'
+        }),
 
-    }),
-  ],
+        new webpack.ProvidePlugin({
+            $: 'jquery',
+            jQuery: 'jquery',
+            'window.jQuery': 'jquery'
+
+        }),
+    ],
 
 }
-
