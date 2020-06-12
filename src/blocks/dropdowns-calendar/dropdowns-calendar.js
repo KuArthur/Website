5d3cd3d018930abf55289f7cde9e1b0ea0467f0e
KuArthur <48932889+KuArthur@users.noreply.github.com>
2020-06-13 02:22:52 +0300

    вношу корректировки в стилях календаря

--- src/blocks/dropdowns-calendar/dropdowns-calendar.js
+++ src/blocks/dropdowns-calendar/dropdowns-calendar.js
@@ -5,3 +5,31 @@ $('.js-dropdowns-calendar__calendar').datepicker({
     prevHtml: "<div>arrow_back</div>",
     nextHtml: "<div>arrow_forward</div>",
 })
+
+
+// $(function() {
+
+//     let $start = $('.js-dropdowns-calendar__date-from'),
+//         $end = $('.js-dropdowns-calendar__date-to');
+
+//     let picker = $('#start').datepicker({
+//         range: true,
+//         multipleDatesSeparator: '-',
+//         language: "en",
+//         clearButton: true,
+//         todayButton: true,
+//         classes: 'abs',
+
+//         onSelect: function(fd, d, picker) {
+//             $(".js-dropdowns-calendar__date-from").val(fd.split("-")[0]);
+//             $("#end").val(fd.split("-")[1]);
+//         },
+//         onHide: function() {
+//             console.log(this);
+//         }
+//     }).data('datepicker');
+
+//     $('.js-dropdowns-calendar__date-to').on('click', () => {
+//         picker.show();
+//     });
+// });
