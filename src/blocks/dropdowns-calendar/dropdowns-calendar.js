0380ddbf5881c829de721a0ffcfe8532a536aec2
KuArthur <48932889+KuArthur@users.noreply.github.com>
2020-06-13 04:07:36 +0300

    немного откорректировал календарь

--- src/blocks/dropdowns-calendar/dropdowns-calendar.js
+++ src/blocks/dropdowns-calendar/dropdowns-calendar.js
@@ -4,6 +4,7 @@ $('.js-dropdowns-calendar__calendar').datepicker({
     inline: true,
     prevHtml: "<div>arrow_back</div>",
     nextHtml: "<div>arrow_forward</div>",
+    clearButton: true
 })
 
 
