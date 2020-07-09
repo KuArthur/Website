26803c900dbb3d64d1cd9f546b0e786d4ae72f90
KuArthur <48932889+KuArthur@users.noreply.github.com>
2020-07-10 00:53:40 +0300

    начал верстку страницы cards, сделал две карточки, возникла проблема с наследованием шаблонов в pug

---
+++ src/pages/cards/cards.js
@@ -0,0 +1,5 @@
+import "../../index.scss";
+import "./cards.scss";
+import "../../blocks/counter/counter";
+import "../../blocks/dropdown/dropdown";
+import "../../blocks/dropdowns-calendar/dropdowns-calendar";
